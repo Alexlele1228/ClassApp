13e9ac013776fd0fbb1771ac24277d0ed078c046
Alexlele <vistatsw@icloud.com>
2020-07-27 15:53:38 +0800

    added clickCounter

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {TextBox} from './components/TextBox';
+import {ClickCounter} from './components/ClickCounter';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.</Text>
+      <TextBox color="green" size="24" Text="123"/>
+      <ClickCounter/>
+      <ClickCounter/>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +23,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
 });
